034b8b06f354516710b91c346edb8ef6f7681663
leabouffaut <lea.bouffaut@hotmail.fr>
2019-10-02 10:39:09 +0200

    Update lea_HPS.m

--- lea_HPS.m
+++ lea_HPS.m
@@ -8,13 +8,11 @@
     %  - spectrogram p,f,t
     %
     % OUTPUTS:
-    %  - f0_HPS: Pitch track from HPS algorithm,
-    %  - time_HPS: time of the HPS pitch track,
-    %  - ampl_HPS: amplitude of the tracks.
+    %  - f0: frequency estimate,
+    %  - time: time vector of the frequency tracks,
+    %  - ampl: amplitude of the tracks.
 
-function [f0_HPS, time_HPS, ampl_HPS] = lea_HPS(p,f,t)
-% function [f0_HPS, time_HPS, ampl_HPS] = lea_HPS(x,fs,fft_size,overlap)
-% [~,f,t,p] = spectrogram(x,hann(fft_size),round((overlap/100)*fft_size),fft_size,fs);
+function [f0, time, ampl] = lea_HPS(p,f,t)
 
 p_HPS = ones(length(f),length(t)); % initailization of p_HPS matrix to 1
 nb_harmo = 2; % number of times the spctrum is downsampeled. In our case is to low to do it more thant twice. 
@@ -36,12 +34,12 @@ end
 f_HPS = linspace(0,50,a)*nb_harmo; %the associated frequency vector
 
 % find Max frequency of the HPS, and the associated amplitude
-f0_HPS = zeros(1,length(t));
-ampl_HPS = zeros(1,length(t));
+f0 = zeros(1,length(t));
+ampl = zeros(1,length(t));
 
 for i = 1:length(t)
     [M,I] = max(p_HPS(:,i));
-    f0_HPS(i) = f_HPS(I);
-    ampl_HPS(i) = sqrt(M);
+    f0(i) = f_HPS(I);
+    ampl(i) = sqrt(M);
 end
-time_HPS = t;
+time = t;
